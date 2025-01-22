8cc658b3976c405349f8dffd57869512a46cf6a3
mcmah309 <dillonmcmahon79@gmail.com>
2025-01-22 18:01:28 +0000

    feat: Macro update

--- src/macros.rs
+++ src/macros.rs
@@ -2,21 +2,21 @@
 #[macro_export]
 macro_rules! bail {
     ($msg:expr) => {
-        return Err(ErrorUnion::new(GenericError::new($msg)));
+        return Err(eros::ErrorUnion::new(eros::GenericError::new($msg)));
     };
 }
 
 #[macro_export]
 macro_rules! generic {
     ($msg:expr) => {
-        ErrorUnion::new(GenericError::new($msg));
+        eros::ErrorUnion::new(eros::GenericError::new($msg));
     };
 }
 
 #[macro_export]
 macro_rules! new {
     ($error:expr) => {
-        ErrorUnion::new(error);
+        eros::ErrorUnion::new(error);
     };
 }
 
