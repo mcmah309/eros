626d3622d456c225d5034ae1dd554eb569e43618
mcmah309 <dillonmcmahon79@gmail.com>
2026-06-13 00:16:47 +0000

    Ignore trybuild in ci

--- rs/eros/tests/trybuild.rs
+++ rs/eros/tests/trybuild.rs
@@ -1,5 +1,7 @@
 
 #[test]
+// We ignore when testing in ci since a different version of rust may be used which give slightly different messages and can become annoying to align
+#[cfg_attr(test_in_ci, ignore)]
 fn trybuild() {
     let t = trybuild::TestCases::new();
     t.compile_fail("tests/trybuild/*.rs");
