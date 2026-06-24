a043dd36d7218e7fa4c18271a81527b35f62a4b4
mcmah309 <dillonmcmahon79@gmail.com>
2026-06-24 03:07:07 +0000

    Update ci with no_std test

--- rs/eros/tests/trybuild.rs
+++ rs/eros/tests/trybuild.rs
@@ -1,5 +1,6 @@
 
 #[test]
+#[cfg_attr(miri, ignore)]
 // We ignore when testing in ci since a different version of rust may be used which give slightly different messages and can become annoying to align
 #[cfg_attr(test_in_ci, ignore)]
 fn trybuild() {
